7976c5618804e7b3f4664a6db53aade6e22134c3
John Dobbs <jtdobbs1@me.com>
2015-10-10 11:55:40 -0400

    Reworked game logic in main.py

---
+++ test_cards.py
@@ -0,0 +1,51 @@
+# coding=utf-8
+# War -> Tests -> Cards
+
+import cards
+
+def test_create_deck():
+    deck = cards.create_deck()
+    assert type(deck) is list
+    assert len(deck) == 52
+    for card in deck:
+        assert type(card) is tuple
+        assert type(card[0]) is int
+        assert type(card[1]) is str
+        assert card[0] <= 13
+        assert card[0] >= 1
+
+def test_shuffle():
+    deck = cards.create_deck()
+    shuf = cards.shuffle_cards(deck)
+    assert deck != shuf
+
+def test_deal_cards():
+    deck = cards.create_deck()
+    inDeck = deck[:]
+    (hum, cpu) = cards.deal_cards(inDeck)
+    assert len(hum) == 26
+    assert len(cpu) == 26
+    assert deck == inDeck
+
+def test_get_top_card():
+    r_deck = cards.create_deck()
+    deck = r_deck[:]
+    card = cards.get_top_card(deck)
+    assert card == r_deck[-1]
+    assert r_deck != deck
+
+def test_get_top_card_multiple():
+    r_deck = cards.create_deck()
+    deck = r_deck[:]
+    hand = cards.get_top_card(deck, n=3)
+    assert len(hand) == 3
+    assert type(hand) is list
+    assert r_deck != deck
+    
+def test_compare_cards_normal_usage():
+    assert cards.compare_cards((4,), (7,)) == 1
+    assert cards.compare_cards((9,), (7,)) == -1
+    assert cards.compare_cards((5,), (5,)) == 0
+    assert cards.compare_cards((1,), (5,)) == -1
+    assert cards.compare_cards((13,), (1,)) == 1
+    assert cards.compare_cards((1,), (1,)) == 0
